3a856252b2d87277b060d56a86f2e44430e8e3a5
Alexey Parfenov <alepar@gmail.com>
2015-10-07 00:55:14 -0700

    working ina219 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,37 @@
 #include "WProgram.h"
 
+#include "Adafruit_INA219.h"
+
+Adafruit_INA219 ina219;
+
 extern "C" int main(void)
 {
-#ifdef USING_MAKEFILE
-
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
-
 	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
-	}
+	digitalWriteFast(13, HIGH);
 
+	Serial.begin(115200);
+	Serial.println("Hello, world!");
 
-#else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
+	ina219.begin();
+
+	while(1) {
+		float shuntvoltage = 0;
+		float busvoltage = 0;
+		float current_mA = 0;
+		float loadvoltage = 0;
+
+		shuntvoltage = ina219.getShuntVoltage_mV();
+		busvoltage = ina219.getBusVoltage_V();
+		current_mA = ina219.getCurrent_mA();
+		loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+		Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+		Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+		Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+		Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+		Serial.println("");
+
+		delay(1000);		
 	}
-#endif
 }
 
